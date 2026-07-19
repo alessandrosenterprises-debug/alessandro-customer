26b0b942b057e9ce75c5f25d0a45e013fa03e6f8
alessandrosenterprises-debug <alessandrosenterprises@gmail.com>
2026-07-19 19:33:54 +0200

    Delete src directory

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all services
-export * from './api-client';
-export * from './auth-service';
-export * from './sync-client';
-export * from './shared-store';
-export * from './notification-service';
