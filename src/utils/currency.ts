5839cdcf564563724551fa7c769ca0abbe097f17
Alessandro Enterprises <alessandrosenterprises@gmail.com>
2026-07-23 21:16:22 +0200

    Delete src directory

--- src/utils/currency.ts
+++
@@ -1,8 +0,0 @@
-/** Formats prices consistently in Zambian Kwacha. */
-export function formatZMW(value: number): string {
-  return new Intl.NumberFormat('en-ZM', {
-    style: 'currency',
-    currency: 'ZMW',
-    minimumFractionDigits: 2,
-  }).format(Number(value) || 0);
-}
